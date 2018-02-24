198b2cb2a5925fc53594758efc4dcfd8e8c45298
Nik Skoufis <n.skoufis@gmail.com>
2018-02-24 09:40:43 +0000

    Add basic form with next button and questions with styles

---
+++ legal-app/src/components/Form.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-container fluid>
+        <v-layout column>
+            <form-question
+                questionNumber="1"
+                questionText="sample question text here"
+                contentText="sample content here"
+            />
+            <next-button
+                v-on:clicked="nextClicked"
+                buttonText="Next"
+            />
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+import FormQuestion from '@/components/FormQuestion'
+import NextButton from '@/components/NextButton'
+
+export default {
+    components: {
+        FormQuestion,
+        NextButton
+    },
+    methods: {
+        nextClicked() {
+            console.log('next clicked');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ legal-app/src/components/FormQuestion.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-container class="container">
+        <div class="question">
+            <span class="question-number">
+                {{ questionNumber }}<v-icon large>arrow_right</v-icon>
+            </span>
+            <span class="question-text">{{ questionText }}</span>
+        </div>
+        <div class="content">
+            <span>{{ contentText }}</span>
+        </div>
+        <div class="input">
+            <input type="text" autocomplete="off"/>
+        </div>
+    </v-container>
+</template>
+
+<script>
+export default {
+    props: ['questionNumber', 'questionText', 'contentText']
+}
+</script>
+
+<style>
+    .container {
+        padding-left: 50px;
+    }
+    .question {
+        padding-top: 50px;
+        padding-bottom: 10px;
+        font-size: 30px;
+    }
+    .question-number {
+        margin-right: 10px;
+    }
+    .question-text {
+        font-weight: 500;
+    }
+    .content {
+        padding-left: 72px;
+        padding-bottom: 10px;
+    }
+    .input {
+        padding-left: 72px;
+        margin-top: 10px;
+    }
+    .input > input {
+        outline-color: initial;
+        outline-style: initial;
+        outline-width: 0px;
+        font-size: 30px;
+    }
+
+</style>

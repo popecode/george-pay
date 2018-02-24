7e6f82a1164ce7d6759d04cdb89d8ffce20ed590
Wong, Nicholas (AU - Melbourne) <nichowong@deloitte.com.au>
2018-02-25 09:14:13 +1100

    Nick integration commit

--- legal-app/src/components/Landing.vue
+++ legal-app/src/components/Landing.vue
@@ -7,10 +7,10 @@
         <v-flex xs12 md4>
           <v-card class="elevation-0">
             <v-card-text class="text-xs-center">
-              <v-icon x-large class="blue--text text--lighten-2">color_lens</v-icon>
+              <v-icon x-large class="blue--text text--lighten-2">announcement</v-icon>
             </v-card-text>
             <v-card-title primary-title class="layout justify-center">
-              <div class="headline text-xs-center">Material Design</div>
+              <div class="headline text-xs-center">Option 1</div>
             </v-card-title>
             <v-card-text>
               Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
@@ -23,10 +23,10 @@
         <v-flex xs12 md4>
           <v-card class="elevation-0 transparent">
             <v-card-text class="text-xs-center">
-              <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
+              <v-icon x-large class="blue--text text--lighten-2">account_balance</v-icon>
             </v-card-text>
             <v-card-title primary-title class="layout justify-center">
-              <div class="headline">Fast development</div>
+              <div class="headline">Option 2</div>
             </v-card-title>
             <v-card-text>
               Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
@@ -39,16 +39,31 @@
         <v-flex xs12 md4>
           <v-card class="elevation-0 transparent">
             <v-card-text class="text-xs-center">
-              <v-icon x-large class="blue--text text--lighten-2">build</v-icon>
+              <v-icon x-large class="blue--text text--lighten-2">assignment</v-icon>
             </v-card-text>
             <v-card-title primary-title class="layout justify-center">
-              <div class="headline text-xs-center">Completely Open Sourced</div>
+              <div class="headline text-xs-center">Option 3</div>
             </v-card-title>
             <v-card-text>
-              <a v-on:click.prevent="hideLanding">Test</a>
-              Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
-              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
-              Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
+              <v-container grid-list-md text-xs-center>
+                <v-layout row wrap>
+                  <v-flex xs6>
+                    <p>Before you start, you need this:</p>
+                    <ul class="document-list">
+                      <li>Document 1</li>
+                      <li>Document 2</li>
+                      <li>Document 3</li>
+                    </ul>
+                  </v-flex>
+                  <v-flex xs6>
+                    <div class="time-container">
+                      <v-icon x-large class="grey--text text--darken-3">alarm</v-icon> 
+                      <span class="time">This takes about</span>
+                      <span class="time actual-time">2 minutes</span>
+                    </div>
+                  </v-flex>
+                </v-layout>
+              </v-container>
             </v-card-text>
           </v-card>
         </v-flex>
@@ -60,44 +75,38 @@
 </template>
 
 <script>
-import ActionCard from '@/components/ActionCard'
 
 export default {
     data() {
         return {
-            actionCards: [
-                {
-                    id: 1,
-                    actionTitle: 'Test1',
-                    actionDescription: 'This is a test description for test 1',
-                    callToAction: 'Test now!',
-                    icon: 'home',
-                    cardColor: 'blue-grey darken-2',
-                    cardClass: 'white--text'
-                },
-                {
-                    id: 2,
-                    actionTitle: 'Test2',
-                    actionDescription: 'This is a test description for test 2',
-                    callToAction: 'Test now!',
-                    icon: 'gavel',
-                    cardColor: 'blue-grey darken-2',
-                    cardClass: 'white--text'
-                },
-                {
-                    id: 3,
-                    actionTitle: 'Test3',
-                    actionDescription: 'This is a test description for test 3',
-                    callToAction: 'Test now!',
-                    icon: 'info',
-                    cardColor: 'blue-grey darken-2',
-                    cardClass: 'white--text'
-                }
-            ]
         }
     },
-    components: {
-        ActionCard
-    }
+    props: [
+      'hideLanding'
+    ]
 }
 </script>
+
+<style>
+  .document-list {
+    margin-left: 20%;
+  }
+
+  .document-list li {
+    text-align: left;
+  }
+
+  .time-container {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .time {
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  .actual-time {
+    font-size: 24px;
+  }
+</style>
--- legal-app/src/components/ProgressMap.vue
+++ legal-app/src/components/ProgressMap.vue
@@ -2,40 +2,44 @@
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
       
-        <v-stepper v-model="e6" vertical>
+        <v-stepper v-model="step_no" vertical>
 
-          <v-stepper-step step="1" :complete="e6 > 1">
+          <v-stepper-step step="1" :complete="step_no > 1">
             Select an app
             <small>Summarize if needed</small>
           </v-stepper-step>
           <v-stepper-content step="1">
-            <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-            <v-btn color="primary" @click.native="e6 = 2">Continue</v-btn>
-            <v-btn flat>Cancel</v-btn>
+            <v-card color="grey lighten-1" class="mb-5" height="200px">
+              Some content goes here
+            </v-card>
+            <v-btn flat>Previous</v-btn>
+            <v-btn color="primary" @click.native="step_no = 2">Next</v-btn>
           </v-stepper-content>
 
-          <v-stepper-step step="2" :complete="e6 > 2">Configure analytics for this app</v-stepper-step>
+          <v-stepper-step step="2" :complete="step_no > 2">Configure analytics for this app</v-stepper-step>
           <v-stepper-content step="2">
-            <Landing v-if="landing"/>
+            <Landing v-if="landing" :hideLanding="hideLanding"/>
             <div v-else>
               <p>form</p>
             </div>
-            <v-btn color="primary" @click.native="e6 = 3">Continue</v-btn>
-            <v-btn flat @click.native="showLanding()">Cancel</v-btn>
+            <v-btn flat @click.native="step_no = 1">Previous</v-btn>
+            <v-btn color="primary" @click.native="step_no = 3">Next</v-btn>
           </v-stepper-content>
 
-          <v-stepper-step step="3" :complete="e6 > 3">Select an ad format and name ad unit</v-stepper-step>
+          <v-stepper-step step="3" :complete="step_no > 3">Select an ad format and name ad unit</v-stepper-step>
           <v-stepper-content step="3">
-            <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-            <v-btn color="primary" @click.native="e6 = 4">Continue</v-btn>
-            <v-btn flat @click.native="e6 = 2">Cancel</v-btn>
+            <v-card color="grey lighten-1" class="mb-5" height="200px">
+              Some content goes here
+            </v-card>
+            <v-btn flat @click.native="step_no = 2">Previous</v-btn>
+            <v-btn color="primary" @click.native="step_no = 4">Next</v-btn>
           </v-stepper-content>
 
-          <v-stepper-step step="4" :complete="e6 > 4">View setup instructions</v-stepper-step>
+          <v-stepper-step step="4" :complete="step_no > 4">View setup instructions</v-stepper-step>
           <v-stepper-content step="4">
             <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-            <v-btn color="primary" @click.native="e6 = 1">Continue</v-btn>
-            <v-btn flat @click.native="e6 = 3">Cancel</v-btn>
+            <v-btn flat @click.native="step_no = 3">Previous</v-btn>
+            <v-btn color="primary" @click.native="step_no = 5">Next</v-btn>
           </v-stepper-content>
 
         </v-stepper>
@@ -50,7 +54,7 @@
   export default {
     data () {
       return {
-        e6: 2,
+        step_no: 1,
         landing: true
       }
     },

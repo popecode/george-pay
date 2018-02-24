5288762164f95e08bb987bbaff4579dfdf67ec7a
Wong, Nicholas (AU - Melbourne) <nichowong@deloitte.com.au>
2018-02-24 20:58:45 +1100

    ?

---
+++ legal-app/src/components/Landing.vue
@@ -0,0 +1,103 @@
+<template>
+
+  <v-flex xs12>
+    <v-container grid-list-xl>
+      <v-layout row wrap align-center>
+
+        <v-flex xs12 md4>
+          <v-card class="elevation-0">
+            <v-card-text class="text-xs-center">
+              <v-icon x-large class="blue--text text--lighten-2">color_lens</v-icon>
+            </v-card-text>
+            <v-card-title primary-title class="layout justify-center">
+              <div class="headline text-xs-center">Material Design</div>
+            </v-card-title>
+            <v-card-text>
+              Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
+              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+              Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
+            </v-card-text>
+          </v-card>
+        </v-flex>
+
+        <v-flex xs12 md4>
+          <v-card class="elevation-0 transparent">
+            <v-card-text class="text-xs-center">
+              <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
+            </v-card-text>
+            <v-card-title primary-title class="layout justify-center">
+              <div class="headline">Fast development</div>
+            </v-card-title>
+            <v-card-text>
+              Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
+              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+              Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
+            </v-card-text>
+          </v-card>
+        </v-flex>
+
+        <v-flex xs12 md4>
+          <v-card class="elevation-0 transparent">
+            <v-card-text class="text-xs-center">
+              <v-icon x-large class="blue--text text--lighten-2">build</v-icon>
+            </v-card-text>
+            <v-card-title primary-title class="layout justify-center">
+              <div class="headline text-xs-center">Completely Open Sourced</div>
+            </v-card-title>
+            <v-card-text>
+              <a v-on:click.prevent="hideLanding">Test</a>
+              Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. 
+              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+              Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti. 
+            </v-card-text>
+          </v-card>
+        </v-flex>
+
+      </v-layout>
+    </v-container>
+  </v-flex>
+
+</template>
+
+<script>
+import ActionCard from '@/components/ActionCard'
+
+export default {
+    data() {
+        return {
+            actionCards: [
+                {
+                    id: 1,
+                    actionTitle: 'Test1',
+                    actionDescription: 'This is a test description for test 1',
+                    callToAction: 'Test now!',
+                    icon: 'home',
+                    cardColor: 'blue-grey darken-2',
+                    cardClass: 'white--text'
+                },
+                {
+                    id: 2,
+                    actionTitle: 'Test2',
+                    actionDescription: 'This is a test description for test 2',
+                    callToAction: 'Test now!',
+                    icon: 'gavel',
+                    cardColor: 'blue-grey darken-2',
+                    cardClass: 'white--text'
+                },
+                {
+                    id: 3,
+                    actionTitle: 'Test3',
+                    actionDescription: 'This is a test description for test 3',
+                    callToAction: 'Test now!',
+                    icon: 'info',
+                    cardColor: 'blue-grey darken-2',
+                    cardClass: 'white--text'
+                }
+            ]
+        }
+    },
+    components: {
+        ActionCard
+    }
+}
+</script>

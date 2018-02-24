ac3c4ad4faf1562d699a8cb532e581d479bfa155
Wong, Nicholas (AU - Melbourne) <nichowong@deloitte.com.au>
2018-02-24 18:33:02 +1100

    Initial progress map component. Updated routes

---
+++ legal-app/src/components/ProgressMap.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <p>test</p>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<style>
+  
+</style>
+
+<style scoped>
+</style>

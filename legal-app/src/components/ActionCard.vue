7c35194349d932e9f22859650533f7a7d8f42c43
Nik Skoufis <n.skoufis@gmail.com>
2018-02-24 08:26:28 +0000

    Add landing page and action card components

---
+++ legal-app/src/components/ActionCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-card :color="cardColor" :class="cardClass" height="500px">
+        <v-container fluid grid-list-lg fill-height>
+            <v-layout justify-space-between column>
+                <v-layout row height="125px">
+                    <v-flex xs7>
+                        <div class="headline">{{ actionTitle }}</div>
+                    </v-flex>
+                    <v-flex xs5>
+                        <v-flex align-center>
+                            <v-icon dark size="100px">{{ icon }}</v-icon>
+                        </v-flex>
+                    </v-flex>
+                </v-layout>
+                <v-layout column justify-space-between>
+                    <div>{{ actionDescription }}</div>
+                    <v-btn flat dark>{{ callToAction }}</v-btn>
+                </v-layout>
+            </v-layout>
+        </v-container>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: ['actionTitle', 'actionDescription', 'callToAction', 'icon', 'cardClass', 'cardColor']
+}
+</script>
---
+++ legal-app/src/components/Landing.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container
+    fluid
+    grid-list-lg
+    style="min-height: 0;"
+  >
+      <v-layout row justify-space-around>
+          <v-flex v-for="actionCard in actionCards" :key="actionCard.id" xs3>
+            <action-card
+                v-bind:actionTitle="actionCard.actionTitle"
+                v-bind:actionDescription="actionCard.actionDescription"
+                v-bind:callToAction="actionCard.callToAction"
+                v-bind:icon="actionCard.icon"
+                v-bind:cardClass="actionCard.cardClass"
+                v-bind:cardColor="actionCard.cardColor"
+            />
+          </v-flex>
+      </v-layout>
+  </v-container>
+</template>
+
+<script>
+import ActionCard from '@/components/ActionCard'
+
+export default {
+    data() {
+        return {
+            actionCards: [
+                {
+                    id: 1,
+                    actionTitle: 'Test1',
+                    actionDescription: 'This is a test description for test 1',
+                    callToAction: 'Test now!',
+                    icon: 'home',
+                    cardColor: 'blue-grey darken-2',
+                    cardClass: 'white--text'
+                },
+                {
+                    id: 2,
+                    actionTitle: 'Test2',
+                    actionDescription: 'This is a test description for test 2',
+                    callToAction: 'Test now!',
+                    icon: 'gavel',
+                    cardColor: 'blue-grey darken-2',
+                    cardClass: 'white--text'
+                },
+                {
+                    id: 3,
+                    actionTitle: 'Test3',
+                    actionDescription: 'This is a test description for test 3',
+                    callToAction: 'Test now!',
+                    icon: 'info',
+                    cardColor: 'blue-grey darken-2',
+                    cardClass: 'white--text'
+                }
+            ]
+        }
+    },
+    components: {
+        ActionCard
+    }
+}
+</script>

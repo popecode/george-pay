05dd600bb06c2f9401b558f44cf7001e2acb6e06
Wong, Nicholas (AU - Melbourne) <nichowong@deloitte.com.au>
2018-02-24 20:55:42 +1100

    WIP

--- legal-app/src/components/ActionCard.vue
+++ legal-app/src/components/ActionCard.vue
@@ -1,23 +1,12 @@
 <template>
     <v-card :color="cardColor" :class="cardClass" height="500px">
-        <v-container fluid grid-list-lg fill-height>
-            <v-layout justify-space-between column>
-                <v-layout row height="125px">
-                    <v-flex xs7>
-                        <div class="headline">{{ actionTitle }}</div>
-                    </v-flex>
-                    <v-flex xs5>
-                        <v-flex align-center>
-                            <v-icon dark size="100px">{{ icon }}</v-icon>
-                        </v-flex>
-                    </v-flex>
-                </v-layout>
-                <v-layout column justify-space-between>
-                    <div>{{ actionDescription }}</div>
-                    <v-btn flat dark>{{ callToAction }}</v-btn>
-                </v-layout>
-            </v-layout>
-        </v-container>
+        <v-card-title primary-title>
+          <div class="headline">{{ actionTitle }}</div>
+          <div>{{ actionDescription }}</div>
+         </v-card-title>
+         <v-card-actions>
+           <v-btn flat dark>{{ callToAction }}</v-btn>
+        </v-card-actions>
     </v-card>
 </template>
 

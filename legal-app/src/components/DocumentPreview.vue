4bbc1920c8538b97a50880b7a8ed3de14b420a82
Nik Skoufis <n.skoufis@gmail.com>
2018-02-25 02:00:10 +0000

    Add document preview component

---
+++ legal-app/src/components/DocumentPreview.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-expansion-panel>
+    <v-expansion-panel-content>
+      <div slot="header">Document preview</div>
+      <v-card>
+        <v-card-text class="document-content">
+<p><strong>
+1/4 Happy Valley</br>
+Clayton VIC 3150</br>
+0403684688
+</strong>
+</p>
+
+<p>
+    <strong>
+Frank’s Burgers</br>
+550 Bourke Street</br>
+Melbourne, VIC 3000
+    </strong>
+</p>
+
+<p><strong>25 Feb 2018</strong><p>
+
+<p>Dear Frank,</p>
+<p>I am writing about a concern I have in relation to certain employee entitlements that I do not believe I have received.</p>
+<p>I was employed by you between <strong>1st Jan 2018</strong> and <strong>3rd March</strong> on a <strong>Part-Time</strong> basis as a Cashier</p>
+<p>I understand that my employment is subject to <strong>Fast Food Industry Award 2010 (MA000003)</strong>. Under <strong>Fast Food Industry Award 2010 (MA000003)</strong>, I believe I am entitled to <strong>$566.70 in wages, $300 in notice payment, $466.30 in annual leave with a total of $2040.84</strong> in outstanding payments.</p>
+<p>I have included documents outlining my entitlements in support of my claim.</p>
+<p>I would appreciate your consideration of this matter. If I do not hear from you within 7 days, I will consider lodging a small claims application with the Victoria Tribunal Court.  Information in relation to your obligations can be found at www.fairwork.gov.au.  Alternatively you may wish to seek independent legal advice.</p>
+<p>I look forward to an early resolution of this matter.</p>
+
+<p>Yours sincerely,</p>
+<p>Tom Jones</p>
+</v-card-text>
+      </v-card>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+    .document-content {
+        padding-left: 40px;
+    }
+</style>

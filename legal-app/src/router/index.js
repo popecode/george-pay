56a0e7a5481cd81af8507e40ff6da73018e3ff3a
Nik Skoufis <n.skoufis@gmail.com>
2018-02-25 01:32:21 +0000

    Add homepage with styling

--- legal-app/src/router/index.js
+++ legal-app/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import ProgressMap from '@/components/ProgressMap'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'ProgressMap',
       component: ProgressMap
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
     }
   ]
 })

bea6428cdc7965de79284a9636612279db3dface
Benjamin Pope <project8en@gmail.com>
2018-02-25 12:51:12 +1100

    Added left drawer menu for preview

--- legal-app/src/App.vue
+++ legal-app/src/App.vue
@@ -1,6 +1,31 @@
 <template>
   <v-app>
+    <v-navigation-drawer
+       persistent
+       v-model="drawer"
+       enable-resize-watcher
+       fixed
+       app
+       clipped
+     >
+       <v-list>
+         <v-list-tile
+           value="true"
+           v-for="(item, i) in items"
+           :key="i"
+         >
+           <v-list-tile-action>
+             <v-icon v-html="item.icon"></v-icon>
+           </v-list-tile-action>
+           <v-list-tile-content>
+             <v-list-tile-title v-text="item.title"></v-list-tile-title>
+           </v-list-tile-content>
+         </v-list-tile>
+       </v-list>
+     </v-navigation-drawer>
+
     <v-toolbar app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
       <v-list-tile-avatar><img :src="avatar"></v-list-tile-avatar>
@@ -16,6 +41,20 @@
 export default {
   data () {
     return {
+      drawer: false,
+       fixed: false,
+       items: [{
+         icon: 'people',
+         title: 'Online User Community'
+      }, {
+        icon: 'account_balance',
+        title: 'Community Legal Centres'
+      },
+      {
+        icon: 'gavel',
+        title: ' Private Lawyers'
+      }
+      ],
       title: 'LawBud',
       username: 'Tom Jones',
       avatar: 'https://vuetifyjs.com/static/doc-images/lists/2.jpg'
